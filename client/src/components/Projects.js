5f7a95d895762423798aee5e23adfa7ec6f45a2c
Edward Treece <ed@edwards-mbp.home>
2024-04-18 18:07:05 +0100

    created new projects container for EasyEats app

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,6 +1,6 @@
 import '../index.css';
 import SpareSeat from '../images/SpareSeat.png';
-
+import EasyEats from '../images/EasyEats.png'
 
 function Projects() {
     return(
@@ -22,7 +22,26 @@ function Projects() {
                                 <a href="https://github.com/andreibadea7/SpareSeat" target="_blank" rel="noreferrer"><button className = "projects--button">Code repository</button></a>
                                 </div>
                         </div> 
-            </div>    
+                        
+            </div>
+
+            <div className ="projects--container">
+                <img src={EasyEats} alt="EasyEats project homepage" className = "projects--images"/>
+                    <div className = "projects--description">
+                        <h4>Description</h4>
+                            <p>Project built over 2 weeks in a group of 3, offering a new marketplace to buy and exchange football tickets.</p>
+                        <h4>Tech stack</h4>
+                            <div className = "projects--tech-stack">
+                                <ul className = "projects--tech-stack-list">
+                                    <li>React</li>
+                                    <li>Node</li>
+                                    <li>MySQL</li>
+                                </ul>
+                                <a href="https://github.com/Treecey98/Restaurant-App" target="_blank" rel="noreferrer"><button className = "projects--button">Code repository</button></a>
+                                </div>
+                        </div> 
+                        
+            </div>        
         </div>
     )
 }
