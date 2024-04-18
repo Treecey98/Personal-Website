343ee90a0033040125eb0f2e1a00bb8e86d41307
Edward Treece <ed@edwards-mbp.home>
2024-04-18 20:11:44 +0100

    editing structure of projects component

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -4,44 +4,46 @@ import EasyEats from '../images/EasyEats.png'
 
 function Projects() {
     return(
-        <div className="main-projects-container">
-            <h2 className = "title--centre" id = "Projects">Projects</h2>
-            <div className ="projects--container">
-                <img src={SpareSeat} alt="SpareSeat project homepage" className = "projects--images"/>
-                    <div className = "projects--description">
-                        <h4>Description</h4>
-                            <p>Project built over 2 weeks in a group of 3, offering a new marketplace to buy and exchange football tickets.</p>
-                        <h4>Tech stack</h4>
-                            <div className = "projects--tech-stack">
-                                <ul className = "projects--tech-stack-list">
-                                    <li>Ruby</li>
-                                    <li>HTML</li>
-                                    <li>CSS</li>
-                                    <li>Javascript</li>
-                                </ul>
-                                <a href="https://github.com/andreibadea7/SpareSeat" target="_blank" rel="noreferrer"><button className = "projects--button">Code repository</button></a>
-                                </div>
-                        </div> 
-                        
-            </div>
+        <div>
+            <div className="main-projects-container">
+                <h2 className = "title--centre" id = "Projects">Projects</h2>
+                <div className ="projects--container">
+                    <img src={SpareSeat} alt="SpareSeat project homepage" className = "projects--images"/>
+                        <div className = "projects--description">
+                            <h4>Description</h4>
+                                <p>Project built over 2 weeks in a group of 3, offering a new marketplace to buy and exchange football tickets.</p>
+                            <h4>Tech stack</h4>
+                                <div className = "projects--tech-stack">
+                                    <ul className = "projects--tech-stack-list">
+                                        <li>Ruby</li>
+                                        <li>HTML</li>
+                                        <li>CSS</li>
+                                        <li>Javascript</li>
+                                    </ul>
+                                    <a href="https://github.com/andreibadea7/SpareSeat" target="_blank" rel="noreferrer"><button className = "projects--button">Code repository</button></a>
+                                    </div>
+                            </div> 
+                            
+                </div>
 
-            <div className ="projects--container">
-                <img src={EasyEats} alt="EasyEats project homepage" className = "projects--images"/>
-                    <div className = "projects--description">
-                        <h4>Description</h4>
-                            <p>Application to help users find restaurants in their local area easier. </p>
-                        <h4>Tech stack</h4>
-                            <div className = "projects--tech-stack">
-                                <ul className = "projects--tech-stack-list">
-                                    <li>React</li>
-                                    <li>Node</li>
-                                    <li>MySQL</li>
-                                </ul>
-                                <a href="https://github.com/Treecey98/Restaurant-App" target="_blank" rel="noreferrer"><button className = "projects--button">Code repository</button></a>
-                                </div>
-                        </div> 
-                        
-            </div>        
+                <div className ="projects--container">
+                    <img src={EasyEats} alt="EasyEats project homepage" className = "projects--images"/>
+                        <div className = "projects--description">
+                            <h4>Description</h4>
+                                <p>Application to help users find restaurants in their local area easier. </p>
+                            <h4>Tech stack</h4>
+                                <div className = "projects--tech-stack">
+                                    <ul className = "projects--tech-stack-list">
+                                        <li>React</li>
+                                        <li>Node</li>
+                                        <li>MySQL</li>
+                                    </ul>
+                                    <a href="https://github.com/Treecey98/Restaurant-App" target="_blank" rel="noreferrer"><button className = "projects--button">Code repository</button></a>
+                                    </div>
+                            </div> 
+                            
+                </div> 
+            </div>       
         </div>
     )
 }
